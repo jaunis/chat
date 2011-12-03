b8162a9b7e13432162d70bb68e27e986ee2398d5
jaunis <Jean.Aunis@eleves.ec-nantes.fr>
2011-12-03 10:47:37 +0100

    developpement des classes communes

--- src/chat/commun/Message.java
+++ src/chat/commun/Message.java
@@ -1,5 +1,55 @@
 package chat.commun;
 
-public class Message {
+import java.util.Date;
 
+public class Message 
+{
+	protected Utilisateur expediteur;
+	protected Date dateEmission;
+	protected String contenu;
+	
+	public Message(String contenu, Utilisateur expediteur)
+	{
+		this.contenu = contenu;
+		this.expediteur = expediteur;
+		this.dateEmission = new Date();
+	}
+	/**
+	 * @return the expediteur
+	 */
+	public Utilisateur getExpediteur() {
+		return expediteur;
+	}
+	/**
+	 * @param expediteur the expediteur to set
+	 */
+	public void setExpediteur(Utilisateur expediteur) {
+		this.expediteur = expediteur;
+	}
+	/**
+	 * @return the dateEmission
+	 */
+	public Date getDateEmission() {
+		return dateEmission;
+	}
+	/**
+	 * @param dateEmission the dateEmission to set
+	 */
+	public void setDateEmission(Date dateEmission) {
+		this.dateEmission = dateEmission;
+	}
+	/**
+	 * @return the contenu
+	 */
+	public String getContenu() {
+		return contenu;
+	}
+	/**
+	 * @param contenu the contenu to set
+	 */
+	public void setContenu(String contenu) {
+		this.contenu = contenu;
+	}
+	
+	
 }
--- src/chat/commun/Utilisateur.java
+++ src/chat/commun/Utilisateur.java
@@ -1,5 +1,38 @@
 package chat.commun;
 
-public class Utilisateur {
-
+public class Utilisateur 
+{
+	protected String id;
+	protected boolean connecte;
+	
+	public Utilisateur(String id)
+	{
+		this.id = id;
+		connecte = true;
+	}
+	/**
+	 * @return the id
+	 */
+	public String getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(String id) {
+		this.id = id;
+	}
+	/**
+	 * @return the connecte
+	 */
+	public boolean isConnecte() {
+		return connecte;
+	}
+	/**
+	 * @param connecte the connecte to set
+	 */
+	public void setConnecte(boolean connecte) {
+		this.connecte = connecte;
+	}
+	
 }
